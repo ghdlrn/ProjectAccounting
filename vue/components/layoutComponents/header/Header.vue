fa48d79dc30e8e5cdf991e8d7fa15707dfd74f19
ghdlrn <ghdlrn@gmail.com>
2024-04-25 00:22:01 +0900

    first commit

--- vue/components/layoutComponents/header/Header.vue
+++ vue/components/layoutComponents/header/Header.vue
@@ -27,10 +27,19 @@
       <v-btn class="bg-success" size="large" append-icon="mdi-account-plus">회원가입</v-btn>
     </NuxtLink>
 
-    <!-- 로그인 버튼 -->
-    <NuxtLink to="/auth/login">
-      <v-btn class="bg-blue-accent-2" size="large" append-icon="mdi-login">로그인</v-btn>
-    </NuxtLink>
+    <!-- 로그인/로그아웃 버튼 -->
+    <template v-if="isLoggedIn">
+      <v-btn class="bg-red-accent-2" size="large" append-icon="mdi-logout" @click="logout">
+        로그아웃
+      </v-btn>
+    </template>
+    <template v-else>
+      <NuxtLink to="/auth/login">
+        <v-btn class="bg-blue-accent-2" size="large" append-icon="mdi-login">
+          로그인
+        </v-btn>
+      </NuxtLink>
+    </template>
 
     <v-spacer></v-spacer>
 
@@ -41,6 +50,18 @@
 import HeaderLogo from "~/components/layoutComponents/header/HeaderLogo.vue";
 import HeaderMenu from "~/components/layoutComponents/header/HeaderMenu.vue";
 import HeaderSearchBar from "~/components/layoutComponents/header/HeaderSearchBar.vue";
+
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '~/stores/auth';
+const router = useRouter();
+const authStore = useAuthStore();
+const isLoggedIn = computed(() => !!authStore.user);
+
+const logout = () => {
+  authStore.logout();
+  router.push('/'); // 리디렉션
+};
 </script>
 
 <style scoped>
