995edc817430d7cf82f65b7aba18fa2b1a15cd54
ghdlrn <ghdlrn@gmail.com>
2024-05-01 23:17:45 +0900

    first commit

--- src/main/java/lkm/starterproject/auth/config/SecurityConfig.java
+++ src/main/java/lkm/starterproject/auth/config/SecurityConfig.java
@@ -55,11 +55,11 @@ public class SecurityConfig {
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration configuration = new CorsConfiguration();
                         configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));    //해당 server port 허용
-                        configuration.setAllowedMethods(Collections.singletonList("*"));        //메소드 허용
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));    //헤더허용
-                        configuration.setMaxAge(3600L);     //허용시간
-                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));        //Authorization헤더 허용
+                        configuration.setAllowedMethods(Collections.singletonList("*"));        //get, post등의 모든 요청 허용
+                        configuration.setAllowCredentials(true);        //쿠키 및 인증데이터 보내도 되는지 허용(자격증명 등에 사용)
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));    //모든 http헤더 허용
+                        configuration.setMaxAge(3600L);     //실행전 요청 응답 캐시할 허용시간(1시간)
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));        //응답에서 브라우저에 노출될 헤더 지정 Authorization헤더
                         return configuration;
                     }
                 })));
@@ -76,13 +76,13 @@ public class SecurityConfig {
                         .anyRequest().authenticated());     //기타 경로는 로그인한 사용자만 사용가능
         //---------------필터---------------------
         http
-                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);    //JWT필터 제일먼저 실행
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);    //JWT필터 제일먼저 실행 토큰을 기반으로 요청을 인증
         http
-                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
-        http    //기존의 필터를 LoginFilter로 대체함, AuthenticationManager()와 JWTUtil 전달
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);   //로그아웃 요청을 처리하여 올바르게 로그아웃되도록 함
+        http    //기존의 필터를 LoginFilter로 대체함, AuthenticationManager()와 JWTUtil 전달, 로그인요청 처리 인증성공시 JWT 생성
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
         http
-                .sessionManagement((session) -> session
+                .sessionManagement((session) -> session                         //세션 관리를 stateless로 구성
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));       //JWT방식 인증/인가 방식은 session을 stateless방식으로 반드시 설정해야함
         return http.build();
     }
