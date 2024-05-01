b25684b5099af6ce772bf2ca16e3c84df9e4fb49
ghdlrn <ghdlrn@gmail.com>
2024-05-01 23:42:45 +0900

    first commit

--- src/main/java/lkm/starterproject/auth/controller/MainController.java
+++ src/main/java/lkm/starterproject/auth/controller/MainController.java
@@ -15,14 +15,14 @@ public class MainController {
     @GetMapping("/")
     public String main() {
 
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();    //SecurityContextHolder에서 인증된 사용자의 이메일을 검색
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); //SecurityContextHolder에서 자격증명 및 권한 검색
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iter = authorities.iterator();
         GrantedAuthority auth = iter.next();
-        String role = auth.getAuthority();
+        String role = auth.getAuthority();      //권한 검색하여 객체에서 권한 이름을 추출
 
         return "User Controller" + email + role;
     }
