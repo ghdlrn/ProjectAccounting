3cb5e3d80326e935eceef05e15d017a45dc70916
ghdlrn <ghdlrn@gmail.com>
2024-05-02 16:17:46 +0900

    first commit

--- src/main/java/lkm/starterproject/accounting/controller/LocalTaxController.java
+++ src/main/java/lkm/starterproject/accounting/controller/LocalTaxController.java
@@ -1,37 +1,24 @@
 package lkm.starterproject.accounting.controller;
 
 import lkm.starterproject.accounting.dto.basic.LocalTaxDto;
-import lkm.starterproject.accounting.dto.basic.TaxOfficeDto;
 import lkm.starterproject.accounting.repository.basic.LocalTaxRepository;
-import lkm.starterproject.accounting.repository.basic.TaxOfficeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
-public class CompanyRegisterController {
+public class LocalTaxController {
 
-    private final TaxOfficeRepository taxOfficeRepository;
     private final LocalTaxRepository localTaxRepository;
 
-    @GetMapping("/register/company-info/tax-office")
-    public List<TaxOfficeDto> getAllTaxOffices() {      //사업장 세무서 조회
-        return taxOfficeRepository.findAll().stream()
-                .map(taxOffice -> new TaxOfficeDto(
-                        taxOffice.getCode(),
-                        taxOffice.getName(),
-                        taxOffice.getJurisdiction()))
-                .collect(Collectors.toList());
-    }
-
     @GetMapping("/register/company-info/local-tax")
-    public List<LocalTaxDto> getAllLocalTax() {      //사업장 세무서 조회
+    public List<LocalTaxDto> getAllLocalTax() {      //지방세 법정동 조회
         return localTaxRepository.findAll().stream()
                 .map(localTax -> new LocalTaxDto(
                         localTax.getCode(),
---
+++ src/main/java/lkm/starterproject/accounting/controller/TaxOfficeController.java
@@ -0,0 +1,30 @@
+package lkm.starterproject.accounting.controller;
+
+import lkm.starterproject.accounting.dto.basic.TaxOfficeDto;
+import lkm.starterproject.accounting.repository.basic.TaxOfficeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class TaxOfficeController {
+
+    private final TaxOfficeRepository taxOfficeRepository;
+
+    @GetMapping("/register/company-info/tax-office")
+    public List<TaxOfficeDto> getAllTaxOffices() {      //사업장 세무서 조회
+        return taxOfficeRepository.findAll().stream()
+                .map(taxOffice -> new TaxOfficeDto(
+                        taxOffice.getCode(),
+                        taxOffice.getName(),
+                        taxOffice.getJurisdiction()))
+                .collect(Collectors.toList());
+    }
+
+}
