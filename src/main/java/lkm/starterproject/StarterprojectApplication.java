c009abcf3ac577dbc5a9ba9fd91d717d69fc3c4f
ghdlrn <ghdlrn@gmail.com>
2024-05-09 00:03:44 +0900

    second commit

--- src/main/java/lkm/starterproject/StarterprojectApplication.java
+++ src/main/java/lkm/starterproject/StarterprojectApplication.java
@@ -3,7 +3,6 @@ package lkm.starterproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class StarterprojectApplication {
 
