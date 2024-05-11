db3679dfb31a68df7aeb97b06b6a8bad353cc643
ghdlrn <ghdlrn@gmail.com>
2024-05-11 18:12:58 +0900

    second commit

--- vue/components/company/CompanyReference.vue
+++ vue/components/company/CompanyReference.vue
@@ -9,6 +9,7 @@ import UiParentCard from "~/components/shared/UiParentCard.vue";
 
 onMounted(() => { store.fetchCompanies(); });
 const company = computed(() => store.companies );
+
 const searchField = ref(['name', 'code']);
 const searchValue = ref('');
 const headers = ref([
@@ -21,6 +22,7 @@ const themeColor = ref('rgb(var(--v-theme-primary))');
 
 const getCompany = (item) => {
   store.getCompany(item.code)
+  menu.value = false;
 };
 
 const deleteCompany = (item) => {
@@ -30,78 +32,91 @@ const deleteCompany = (item) => {
 };
 
 const dialog = ref(false);
+const menu = ref(false);
 
 </script>
 
 <template>
-
-  <UiParentCard title="회사 조회" class="reference">
-  <PerfectScrollbar>
-      <v-card>
-        <v-card-item>
-
-          <v-row justify="space-between" class="align-center">
-            <v-col cols="6">
-              <v-text-field
-                  type="text"
-                  variant="outlined"
-                  color="primary"
-                  persistent-placeholder
-                  placeholder="회사명 검색"
-                  v-model="searchValue"
-                  hide-details>
-                <template v-slot:prepend-inner>
-                  <SearchOutlined :style="{ fontSize: '14px' }" />
-                </template>
-              </v-text-field>
-            </v-col>
-            <v-col cols="3" offset="3">
-              <div class="d-flex gap-2 justify-end">
-                <v-dialog v-model="dialog" class="register">
-                  <template v-slot:activator="{ props }">
-                    <v-btn variant="flat" color="primary" v-bind="props" size="large">
-                      <template v-slot:prepend>
-                        <PlusOutlined />
+  <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :close-on-click="true"
+      lazy
+      transition="scale-transition"
+      offset-y
+      min-width="290px">
+    <template v-slot:activator="{ props }">
+      <UiParentCard title="회사 조회" class="reference" v-bind="props">
+      <PerfectScrollbar>
+          <v-card>
+            <v-card-item>
+              <v-row justify="space-between" class="align-center">
+                <v-col cols="6">
+                  <v-text-field
+                      type="text"
+                      variant="outlined"
+                      color="primary"
+                      persistent-placeholder
+                      placeholder="회사명 검색"
+                      v-model="searchValue"
+                      hide-details>
+                    <template v-slot:prepend-inner>
+                      <SearchOutlined :style="{ fontSize: '14px' }" />
+                    </template>
+                  </v-text-field>
+                </v-col>
+                <v-col cols="3" offset="3">
+                  <div class="d-flex gap-2 justify-end">
+                    <v-dialog v-model="dialog" class="register">
+                      <template v-slot:activator="{ props }">
+                        <v-btn variant="flat" color="primary" v-bind="props" size="large">
+                          <template v-slot:prepend>
+                            <PlusOutlined />
+                          </template>
+                          회사 등록
+                        </v-btn>
                       </template>
-                      회사 등록
-                    </v-btn>
-                  </template>
-                  <CompanyRegister />
-                </v-dialog>
-              </div>
-            </v-col>
-          </v-row>
+                      <CompanyRegister />
+                    </v-dialog>
+                  </div>
+                </v-col>
+              </v-row>
 
-        </v-card-item>
-        <v-divider></v-divider>
-        <v-card-text class="pa-0">
-          <EasyDataTable
-              :headers="headers"
-              :items="company"
-              table-class-name="customize-table"
-              :theme-color="themeColor"
-              :search-field="searchField"
-              :search-value="searchValue"
-              @click-row="getCompany"
-              :rows-per-page="10">
-            <template #item-operation="item">
-              <div class="operation-wrapper">
-                <v-btn icon color="secondary" variant="text" rounded>
-                  <EyeOutlined />
-                </v-btn>
-                <v-btn icon color="primary" variant="text" rounded>
-                  <EditOutlined />
-                </v-btn>
-                <v-btn icon color="error" variant="text" @click="deleteCompany(item)" rounded>
-                  <DeleteOutlined />
-                </v-btn>
-              </div>
-            </template>
-          </EasyDataTable>
-        </v-card-text>
-      </v-card>
-  </PerfectScrollbar>
-  </UiParentCard>
+            </v-card-item>
+            <v-divider></v-divider>
+            <v-card-text class="pa-0">
+              <EasyDataTable
+                  :headers="headers"
+                  :items="company"
+                  table-class-name="customize-table"
+                  :theme-color="themeColor"
+                  :search-field="searchField"
+                  :search-value="searchValue"
+                  @click-row="getCompany"
+                  :rows-per-page="10">
+                <template #item-operation="item">
+                  <div class="operation-wrapper">
+                    <v-btn icon color="secondary" variant="text" rounded>
+                      <EyeOutlined />
+                    </v-btn>
+                    <v-btn icon color="primary" variant="text" rounded>
+                      <EditOutlined />
+                    </v-btn>
+                    <v-btn icon color="error" variant="text" @click="deleteCompany(item)" rounded>
+                      <DeleteOutlined />
+                    </v-btn>
+                  </div>
+                </template>
+              </EasyDataTable>
+            </v-card-text>
+          </v-card>
+        </PerfectScrollbar>
+      </UiParentCard>
+      </template>
+
+    <CompanyUpdate />
+
+  </v-menu>
 </template>
 
 <style lang="scss">
