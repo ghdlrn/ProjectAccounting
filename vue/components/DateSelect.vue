9c6b9aab2db3f27945d7afc491c6e89f74aeed78
ghdlrn <ghdlrn@gmail.com>
2024-05-12 18:46:03 +0900

    second commit

--- vue/components/DateSelect.vue
+++ vue/components/DateSelect.vue
@@ -7,7 +7,6 @@
         lazy
         transition="scale-transition"
         offset-y
-        full-width
         min-width="290px">
       <template v-slot:activator="{ props }">
         <v-text-field
@@ -23,7 +22,7 @@
       </template>
         <DatePicker
             v-model="innerDate"
-            @input="updateDate" />
+            @change="updateDate" />
     </v-menu>
   </div>
 </template>
@@ -35,15 +34,18 @@ import {DatePicker} from "v-calendar";
 const props = defineProps({
   modelValue: {
     type: String,
-    default: () => new Date().toISOString().slice(0, 10)  // 기본값은 오늘 날짜
+    default: () => new Date().toISOString().slice(0, 10)
   }
 });
 
 const emit = defineEmits(['update:modelValue']);
 const menu = ref(false);
-const innerDate = ref(new Date(props.modelValue));  // 문자열 날짜를 Date 객체로 변환
+const innerDate = ref(new Date(props.modelValue));
 
 const formattedDate = computed(() => {
+  nextTick(() => {
+    menu.value = false;
+  });
   if (!innerDate.value) return '';
   const d = new Date(innerDate.value);
   return `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')}`;
@@ -51,10 +53,8 @@ const formattedDate = computed(() => {
 
 function updateDate(value) {
   innerDate.value = new Date(value);
-  emit('update:modelValue', innerDate.value.toISOString().slice(0, 10));  // ISO 문자열의 날짜 부분만 발송
-  nextTick(() => {
-    menu.value = false;
-  });
+  emit('update:modelValue', innerDate.value.toISOString().slice(0, 10));
+
 }
 
 watch(innerDate, (newValue) => {
