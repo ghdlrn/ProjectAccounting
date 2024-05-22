196a5bab587213cdbee1ba6eb10448b7fafa4a1d
ghdlrn <ghdlrn@gmail.com>
2024-05-22 11:55:45 +0900

    second commit

--- vue/components/register/account-title-compendium/AccountTitleReference.vue
+++ vue/components/register/account-title-compendium/AccountTitleReference.vue
@@ -1,11 +1,151 @@
-<script setup lang="ts">
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import { useAccountTitleStore } from "~/stores/accounting/account-title.ts";
+const store = useAccountTitleStore();
 
+import {DeleteOutlined, PlusOutlined, SearchOutlined, ShoppingCartOutlined, DollarOutlined } from "@ant-design/icons-vue";
+import UiParentCard from "~/components/shared/UiParentCard.vue";
+import AccountTitleRegister from "~/components/register/account-title-compendium/AccountTitleRegister.vue";
+import AccountTitleUpdate from "~/components/register/account-title-compendium/AccountTitleUpdate.vue";
+
+onMounted(() => { store.fetchAccountTitle(); });
+const accountTitle = computed(() => store.accountTitle );
+
+const searchField = ref(['id', 'name', 'accountTitleNumber', 'division', 'useStatus']);
+const searchValue = ref('');
+const headers = ref([
+  { text: '코드', value: 'code', sortable: true },
+  { text: '계정과목', value: 'name', sortable: true },
+  { text: '대차구분', value: 'balanceClassification', sortable: true },
+  { text: '사용여부', value: 'useStatus', sortable: true },
+  { text: '삭제', value: 'operation' }
+]);
+const themeColor = ref('rgb(var(--v-theme-primary))');
+
+const selectedAccountTitle = ref(null);
+
+const getAccountTitle = (item) => {
+  store.getAccountTitle(item.id).then(accountTitleData => {
+    selectedAccountTitle.value = accountTitleData;
+  });
+};
+
+const deleteAccountTitle = (item) => {
+  if (confirm("정말로 삭제하시겠습니까?")) {
+    store.deleteAccountTitle(item.id)
+  }
+};
+
+const dialog = ref(false);
+const menu = ref(false);
 </script>
 
 <template>
+  <UiParentCard title="계정과목 조회" class="reference" >
+    <PerfectScrollbar>
+      <v-card>
+        <v-card-item>
+          <v-row justify="space-between" class="align-center">
+            <v-col cols="6">
+              <v-text-field
+                  type="text"
+                  variant="outlined"
+                  color="primary"
+                  persistent-placeholder
+                  placeholder="계정과목 검색"
+                  v-model="searchValue"
+                  hide-details>
+                <template v-slot:prepend-inner>
+                  <SearchOutlined :style="{ fontSize: '14px' }" />
+                </template>
+              </v-text-field>
+            </v-col>
+            <v-col cols="3" offset="3">
+              <div class="d-flex gap-2 justify-end">
+                <v-dialog v-model="dialog" class="register">
+                  <template v-slot:activator="{ props }">
+                    <v-btn variant="flat" color="primary" v-bind="props" size="large">
+                      <template v-slot:prepend>
+                        <PlusOutlined />
+                      </template>
+                      계정과목 등록
+                    </v-btn>
+                  </template>
+                  <template v-if="dialog">
+                    <AccountTitleRegister @closeDialog="dialog = false" />  <!-- 수정된 부분 -->
+                  </template>
+                </v-dialog>
+              </div>
+            </v-col>
+          </v-row>
 
+        </v-card-item>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="pa-0">
+          <v-menu
+              v-model="menu"
+              :close-on-content-click="false"
+              lazy
+              transition="scale-transition"
+              origin="top right">
+            <template v-slot:activator="{ props }">
+              <EasyDataTable
+                  v-bind="props"
+                  :headers="headers"
+                  :items="accountTitle"
+                  table-class-name="customize-table"
+                  :theme-color="themeColor"
+                  :search-field="searchField"
+                  :search-value="searchValue"
+                  @click-row="getAccountTitle"
+                  :rows-per-page="10">
+                <template v-slot:item-division="{ division }">
+                  <v-chip color="success" v-if="division === '매입'" size="small" label variant="outlined">
+                    <template v-slot:prepend>
+                      <v-icon icon="mdi-cart" />
+                    </template>
+                    매입
+                  </v-chip>
+                  <v-chip color="primary" v-if="division === '매출'" size="small" label variant="outlined">
+                    <template v-slot:prepend>
+                      <v-icon icon="mdi-cash" />
+                    </template>
+                    매출
+                  </v-chip>
+                </template>
+                <template v-slot:item-useStatus="{ useStatus }">
+                  <v-chip color="success" v-if="useStatus === 'USE'" size="small" label> 사용 </v-chip>
+                  <v-chip color="error" v-if="useStatus === 'UNUSED'" size="small" label> 미사용 </v-chip>
+                </template>
+                <template v-slot:item-operation="item">
+                  <div class="operation-wrapper">
+                    <v-btn icon color="error" variant="text" @click.stop="deleteAccountTitle(item)" rounded>
+                      <DeleteOutlined  />
+                    </v-btn>
+                  </div>
+                </template>
+              </EasyDataTable>
+            </template>
+            <AccountTitleUpdate :customer="selectedAccountTitle" @closeDialog="menu = false"/>
+          </v-menu>
+        </v-card-text>
+      </v-card>
+    </PerfectScrollbar>
+  </UiParentCard>
 </template>
 
 <style scoped lang="scss">
-
+.reference {
+  width: 40%;
+  min-width: 900px;
+  min-height: 850px;
+}
+.register {
+  width: 50%;
+  height: 80%;
+  min-width: 1000px;
+  min-height: 1300px;
+}
 </style>
