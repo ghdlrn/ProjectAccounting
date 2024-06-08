c3e14a38cd46401c2a1560b593185983846922e8
ghdlrn <ghdlrn@gmail.com>
2024-06-08 23:21:06 +0900

    second commit

---
+++ vue/types/accounting/normal-document.ts
@@ -0,0 +1,13 @@
+import type {AccountTitle} from "~/types/accounting/account-title";
+import type {Customer} from "~/types/accounting/customer";
+import type {Company} from "~/types/accounting/company";
+
+export interface Finance {
+    company: Company;
+    date: Date;     //일자
+    division: string;    //구분
+    accountTitle: AccountTitle;
+    customer: Customer;
+    debit: number;
+    credit: number;
+}
