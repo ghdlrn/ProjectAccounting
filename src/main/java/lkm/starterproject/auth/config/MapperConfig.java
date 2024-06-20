7a7d7569b607e39d5a3064b93b3d51ee6b000d95
ghdlrn <ghdlrn@gmail.com>
2024-06-20 16:03:35 +0900

    first

---
+++ src/main/java/lkm/starterproject/auth/config/MapperConfig.java
@@ -0,0 +1,15 @@
+package lkm.starterproject.auth.config;
+
+import lkm.starterproject.accounting.mapper.company.CompanyMapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public CompanyMapper companyMapper() {
+        return Mappers.getMapper(CompanyMapper.class);
+    }
+}
